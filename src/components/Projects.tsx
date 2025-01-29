0053deaae4aeed7f6dffc34ed5a5aad1c9eca298
Ian Hoe <ianhoe@gmail.com>
2025-01-29 16:01:21 +0800

    adjusted project links and boxes

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -8,48 +8,49 @@ const Projects = () => {
       <h2 className="mb-4 display-4 fw-bold">Projects</h2>
       <div className="row">
         <div className="col-md-6">
-          <div className="card shadow-sm">
-            <a href="https://github.com/eohnai/CS203-Project" target="_blank" rel="noopener noreferrer" className="no-link-style mx-2">
+          <a href="https://github.com/eohnai/CS203-Project" target="_blank" rel="noopener noreferrer" className="no-link-style mx-2">
+            <div className="card shadow-sm">
               <img src={Tetris} className="card-img-top" alt="Project 1" />
-              <div className="card-body">
-                <h5 className="card-title fw-bold">Tetra League | Spring Boot, React, MongoDB</h5>
-                <p className="card-text">
-                  <ul className="list-unstyled">
-                    <li>
-                      Developed a tournament management system for Tetris, handling player
-                      registration, match scheduling, and live leaderboard tracking.
-                    </li>
-                    <li>
-                      Implemented an Elo-based ranking system to match players of similar
-                      skill levels for fair gameplay.
-                    </li>
-                  </ul>
-                </p>
-              </div>
-            </a>
-          </div>
+                <div className="card-body">
+                  <h5 className="card-title fw-bold">Tetra League</h5>
+                  <h6 className="card-subtitle mb-2 text-muted">Spring Boot, React, MongoDB</h6>
+                  <p className="card-text">
+                    <ul className="list-unstyled">
+                      <li>
+                        Developed a tournament management system for Tetris, handling player
+                        registration, match scheduling, and live leaderboard tracking.
+                      </li>
+                      <li>
+                        Implemented an Elo-based ranking system to match players of similar
+                        skill levels for fair gameplay.
+                      </li>
+                    </ul>
+                  </p>
+                </div>
+            </div>
+          </a>
         </div>
 
         <div className="col-md-6">
-          <div className="card shadow-sm">
-            <a href="https://github.com/eohnai/FinalProjectCrazyEight" target="_blank" rel="noopener noreferrer" className="no-link-style mx-2">
+          <a href="https://github.com/eohnai/FinalProjectCrazyEight" target="_blank" rel="noopener noreferrer" className="no-link-style mx-2">
+            <div className="card shadow-sm">
               <img src={CrazyEights} className="card-img-top" alt="Project 2" />
-              <div className="card-body">
-                <h5 className="card-title fw-bold">Crazy Eights | Java Swing</h5>
-                <p className="card-text">
-                  <ul className="list-unstyled">
-                    <li>
-                      Developed a Java Swing version of the classic Crazy Eights card
-                      game.
-                    </li>
-                    <li>
-                      Designed and created an interactive graphical user interface (GUI).
-                    </li>
-                  </ul>
-                </p>
-              </div>
-            </a>
-          </div>
+                <div className="card-body">
+                  <h5 className="card-title fw-bold">Crazy Eights</h5>
+                  <h6 className="card-subtitle mb-2 text-muted">Java Swing</h6>
+                  <p className="card-text">
+                    <ul className="list-unstyled">
+                      <li>
+                        Developed a Java Swing version of the classic Crazy Eights card game.
+                      </li>
+                      <li>
+                        Designed an interactive graphical user interface (GUI).
+                      </li>
+                    </ul>
+                  </p>
+                </div>
+            </div>
+          </a>
         </div>
       </div>
     </div>
